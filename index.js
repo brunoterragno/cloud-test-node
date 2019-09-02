3ec19d450a836639f57f0ccd0133314b9e07d183
Bruno Terragno <bruno.terragno@gmail.com>
2019-09-01 22:49:29 -0300

    Add AI

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const http = require("http");
+const appInsights = require("applicationinsights");
+
+appInsights.setup("db270688-e61c-4de1-8339-c05edc20c0c3");
+appInsights.start();
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/plain" });
